32ddc23710a586bcc383e7cdc77273ec815e3eb6
fellipemanaia <fellipe.manaia@gmail.com>
2026-05-05 17:27:55 -0300

    :tada: init: Inicio do Sistema de Lanchonete

---
+++ projetoLanchonete/IDesconto.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace projetoLanchonete
+{
+    public interface IDesconto
+    {
+        public decimal AplicarDesconto(decimal valor){
+            
+        }
+    }
+}
---
+++ projetoLanchonete/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace projetoLanchonete;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Bebida b1=new Bebida();
+        Lanche l1=new Lanche();
+        Sobremesa s1=new Sobremesa();
+        Console.WriteLine($"Digite o nome do Lanche: ");
+        l1.nome=Console.ReadLine();
+        Console.WriteLine($"Digite o nome da Bebida:");
+        b1.nome=Console.ReadLine();
+        Console.WriteLine($"Digite o nome da Sobremesa: ");
+        s1.nome=Console.ReadLine();
+        
+
+        Console.WriteLine($"Digite o preço do Lanche: ");
+        l1.precoBase=int.Parse(Console.ReadLine());
+        Console.WriteLine($"Digite o preço da Bebida:");
+        b1.precoBase=int.Parse(Console.ReadLine());
+        Console.WriteLine($"Digite o preço da Sobremesa: ");
+        s1.precoBase=int.Parse(Console.ReadLine());
+        l1.precoBase+=2;
+        AplicarDesconto(s1.precoBase);
+    }
+}
